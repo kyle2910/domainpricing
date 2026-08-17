56d825f113419fdddd1121eff6656eed698284f4
Nguyen Huu Hoa <kyle.zst@proton.me>
2026-08-17 14:20:20 +0700

    2026-08-17

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Used to generate absolute canonical URLs, OG tags, and sitemap.xml.
   // Change this to your real production domain before deploying.
-  site: 'https://domainpricing.net',
+  site: 'https://domainpricing.pages.dev',
 });
